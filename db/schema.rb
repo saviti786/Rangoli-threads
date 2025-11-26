8450a5e948e9f1714d018f1170eba9ffe5e3ec20
saviti786 <saviti@academic.rrc.ca>
2025-11-26 11:54:12 -0600

    Category model created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_26_174817) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_26_175314) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
